8abab8a9b7c25744524ccc1ff1545d7d2beb3c45
majhianish <majhianish123@gmail.com>
2023-07-13 21:07:05 +0530

    modified:   backend/index.js 	modified:   backend/node_modules/.package-lock.json

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,38 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
 const { generateFile } = require('./generateFile');
 const { executePy } = require('./executePy');
-const cors = require('cors');
+const Job = require('./models/Jobs')
+
+mongoose.connect("mongodb://localhost/mern").
+    catch(err => handleError(err));
 
 const app = express();
 
-app.use(cors( ));
+app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    return res.json({hello: "world"});
+app.get('/status', async (req, res) => {
+    const jobId = req.query.id;
+    console.log('status requested', jobId);
+
+    if (jobId === undefined) {
+        return res.status(400).json({success: false, error: 'Missing query ID'});
+    }
+
+    try {
+        const job = await Job.findById(jobId);
+        if (job === undefined) {
+            return res.status(404).json({success: false, error: 'invalid job Id'});
+        }
+
+        return res.status(200).json({success: true, job});
+    } catch (err) {
+        return res.status(400).json({success: true, error: JSON.stringify(err)});
+    }
 });
 
 app.post('/run', async (req, res) => {
@@ -26,12 +48,35 @@ app.post('/run', async (req, res) => {
 
     // const filepath = '', output = '';
 
+    let job;
+
     try {
         filepath = await generateFile(language, code);
+
+        job = await new Job({language, filepath}).save();
+        const jobId = job['_id'];
+        console.log(job);
+
+        res.status(201).json({success: true, jobId});
+
+        job['startedAt'] = new Date();
+
         output = await executePy(filepath);
-        return res.json({ filepath, output });
+
+        job['completedAt'] = new Date();
+        job['status'] = 'success';
+        job['output'] = output;
+
+        await job.save();
+        console.log(job);
+        // return res.json({ filepath, output });
     } catch (err) {
-        res.status(500).json({err});
+        job['completedAt'] = new Date();
+        job['status'] = 'error';
+        job['output'] = JSON.stringify(err);
+        await job.save();
+        console.log(job);
+        // res.status(500).json({err});
     }
 
 });
--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ function App() {
 
   const [code, setCode] = useState('');
   const [output, setOutput] = useState('');
+  const [status, setStatus] = useState('');
+  const [jobId, setJobId] = useState('');
 
   const handleSubmit = async () => {
 
@@ -15,20 +17,52 @@ function App() {
     };
 
     try {
+      setJobId('');
+      setStatus('');
+      setOutput('');
       const { data } = await axios.post("http://localhost:5000/run", payload);
-      setOutput(data.output);
-    } catch (err) {
-      console.log(err.response);
+      console.log(data);
+      setJobId(data.jobId);
+      let intevalId;
+
+      intevalId = setInterval(async () => {
+        const { data: dataRes } = await axios.get('http://localhost:5000/status', { params: { id: data.jobId } });
+        const {success, job, error} = dataRes;
+        console.log(dataRes);
+
+        if (success) {
+          const {status: jobStatus, output: jobOutput} = job;
+          setStatus(jobStatus);
+          if (jobStatus === 'pending') return;
+          setOutput(jobOutput);
+          clearInterval(intevalId);
+        } else {
+          setStatus('Error: Please retry!');
+          console.error(error);
+          clearInterval(intevalId);
+          setOutput(error);
+        }
+
+        console.log(dataRes);
+
+      }, 1000);
+
+    } catch ({ response }) {
+      const errMsg = response.data.err.stderr;
+      if (response) setOutput(errMsg);
+      else setOutput('Error connecting to server!');
     }
   }
 
   return (
     <div className="App">
       <h1>Judge0 clone</h1>
-      <textarea rows='20' cols='75' value= {code} onChange={(e) => {setCode(e.target.value)}}></textarea>
-      <br/>
+      <textarea rows='20' cols='75' value={code} onChange={(e) => { setCode(e.target.value) }}></textarea>
+      <br />
       <button onClick={handleSubmit}>Submit</button>
-      <br/>
+      <br />
+      <p>{status}</p>
+      <p>{jobId && `JobID: ${jobId}`}</p>
       <p> {output} </p>
     </div>
   );
